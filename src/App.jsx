32ae2a4437c4bfbf52a7e5cd864b0ec3c1bb1036
erlopez11 <lopezelainy@gmail.com>
2025-03-27 22:19:31 -0500

    wip: add fetchData fn that uses starshipService to log starshup data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
+import * as starshipService from './services/starshipService';
 import './App.css'
 
 function App() {
+  const [starshipData, setStarshipData] = useState([]);
+  const [displayedStarships, setDisplayedStarships] = useState([]);
+
+  const fetchData = async () => {
+    const data = await starshipService.index(11);
+    console.log(data);
+  };
+
 
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>StarWars Starships</h1>
+      <button onClick={fetchData}>Fetch Starship Data</button>
+    </>
   )
 }
 
